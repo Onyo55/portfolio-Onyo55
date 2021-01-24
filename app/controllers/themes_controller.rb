cacea844fcfcd64fce265de35edb11dd99eb274a
Onyo55 <dayeyada@gmail.com>
2021-01-24 23:33:09 +0900

    themas#create編集

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,4 +1,6 @@
 class ThemesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
@@ -7,6 +9,21 @@ class ThemesController < ApplicationController
   end
 
   def create
+    @theme = Theme.new(theme_params)
+    if @theme.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def theme_params
+    params.require(
+      :theme
+    ).permit(
+      :main_theme,:sub_theme_1, :sub_theme_2,
+    ).merge(user_id: current_user.id)
+  end
 end
