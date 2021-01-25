890510356ce5570f0150d1ad4415cac9bcf6c866
Onyo55 <dayeyada@gmail.com>
2021-01-25 15:00:46 +0900

    誤字修正

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme < ApplicationRecord
   with_options presence: true do
-    validates :mail_theme
+    validates :main_theme
     validates :sub_theme_1
     validates :sub_theme_2
   end
