dbdead385f208d4cd809ecc0f6498c39a0d1a125
Onyo55 <dayeyada@gmail.com>
2021-01-26 18:07:28 +0900

    アソシエーション設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  validates :text, presence: true
+  validates :sub_theme_num, numericality: { only_integer: true }
+  
+  belongs_to :user
+  belongs_to :theme
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
     validates :email,                 uniqueness: { case_sensitive: false }
     validates :password,              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   end
+
+  has_many :themes
+  has_many :comments
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
