f6fbadc0a7f06534fd6701a5bc67dbd04da2850a
Onyo55 <dayeyada@gmail.com>
2021-01-26 17:13:04 +0900

    commentsテーブル作成

--- db/migrate/20210126075524_create_comments.rb
+++ db/migrate/20210126075524_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.string      :text,          null: false
+      t.integer     :sub_theme_num, null: false
+      t.references  :user,          foreign_key:true
+      t.references  :theme,         foreign_key:true
       t.timestamps
     end
   end
