082be62cf0206db68b7bdf7c1965c4d74d63219d
Onyo55 <dayeyada@gmail.com>
2021-02-02 15:45:55 +0900

    投稿フォームの色変え機能追加

---
+++ app/javascript/comments/post.js
@@ -0,0 +1,23 @@
+function post(){
+  const subThemeBtn = document.getElementsByName("sub-theme-btn")
+  const postTextArea = document.getElementById("post-text")
+  console.log(postTextArea)
+  subThemeBtn.forEach(function(e){
+    e.addEventListener('click',function(){
+      console.log(e.value)
+      switch(e.value){
+        case "1":
+          postTextArea.setAttribute("data-color", "blue")
+          break;
+        case "2":
+          postTextArea.setAttribute("data-color", "red")
+          break;
+        case "3":
+          postTextArea.setAttribute("data-color", "pur")
+          break;
+      }
+    })
+  }) 
+}
+
+window.addEventListener('load', post)
