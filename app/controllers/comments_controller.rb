839277c8bb25a6e976bd046a11f6b3253d5ef714
Onyo55 <dayeyada@gmail.com>
2021-02-04 16:36:23 +0900

    テーマ詳細ページのレイアウト修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,8 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     render json:{comment: comment,
-      user_name: comment.user.name,
-    }
+                  user_name: comment.user.name,
+                }
   end
 
   private
