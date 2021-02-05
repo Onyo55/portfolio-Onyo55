05abdfe5b87e8baba696475730bda797a41f5cd0
Onyo55 <dayeyada@gmail.com>
2021-02-05 18:00:31 +0900

    Themeモデル単体テストコード実装

--- spec/factories/themes.rb
+++ spec/factories/themes.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :theme do
+    main_theme    {Faker::String.random(length: 1..50)}
+    sub_theme_1   {Faker::String.random(length: 1..20)}
+    sub_theme_2   {Faker::String.random(length: 1..20)}
+
+    association :user
   end
 end
--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Theme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @theme = FactoryBot.build(:theme)
+  end
+
+  describe 'テーマ投稿機能' do
+    context 'テーマ情報が保存できるとき' do
+      it '@themeが適切な値を持つとき保存できる' do
+        expect(@theme).to be_valid
+      end
+    end
+
+    context 'テーマ情報が保存できないとき' do
+      # 以下、特定の値が空の場合
+      it 'main_themeが空だと登録できない'do
+        @theme.main_theme = ''
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("メインテーマを入力してください")
+      end
+
+      it 'sub_theme_1が空だと登録できない'do
+        @theme.sub_theme_1 = ''
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("サブテーマ1を入力してください")
+      end
+
+      it 'sub_theme_2が空だと登録できない'do
+        @theme.sub_theme_2 = ''
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("サブテーマ2を入力してください")
+      end
+      #以下、文字数制限のテスト
+      it 'main_themeが50文字より多いと登録できない'do
+        @theme.main_theme = "a" * 51
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("メインテーマは50文字以内で入力してください")
+      end
+
+      it 'sub_theme_1が20文字より多いと登録できない'do
+        @theme.sub_theme_1 = 'a' * 21
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("サブテーマ1は20文字以内で入力してください")
+      end
+
+      it 'main_theme_2が20文字より多いと登録できない'do
+        @theme.sub_theme_2 = 'a' * 21
+        @theme.valid?
+        expect(@theme.errors.full_messages).to include("サブテーマ2は20文字以内で入力してください")
+      end
+    end
+
+  end
 end
