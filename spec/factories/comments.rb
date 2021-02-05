f8fbb0dd3cf9b7c092271c8920d1c9178dc32765
Onyo55 <dayeyada@gmail.com>
2021-02-05 18:47:06 +0900

    Commentモデルの単体テストコード実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :comment do
+    text              {Faker::String.random(length: 1..200)}
+    sub_theme_num     {Faker::Number.within(range: 1..3)}
+
+    association :user
+    association :theme
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿機能' do
+    context 'コメントが保存できる時' do
+      it '適切な値が入力されている時保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントが保存できない時' do
+      it 'textが空の時は登録できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("テキストを入力してください")
+      end
+
+      it 'sub_theme_numが空の時は登録できない' do
+        @comment.sub_theme_num = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("サブテーマを入力してください")
+      end
+
+      it 'sub_theme_numが1~3以外の時は登録できない' do
+        @comment.sub_theme_num = 4
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("サブテーマは一覧にありません")
+      end
+
+      it 'textが200文字を超えるの時は登録できない' do
+        @comment.text = 'a' * 201
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("テキストは200文字以内で入力してください")
+      end
+
+      it 'userが紐付いていないときは登録できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Userを入力してください")
+      end
+
+      it 'themeが紐付いていないときは登録できない' do
+        @comment.theme = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Themeを入力してください")
+      end
+    end
+
+  end
 end
